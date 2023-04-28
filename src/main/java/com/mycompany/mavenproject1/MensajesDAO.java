e4c8454410c5cc1a1c8f3240af30d522183ae259
Brayan Acosta <landakkpp@gmail.com>
2023-04-28 00:35:40 -0500

    agregando en mensajeService logica para crear un mensaje, realizando correpciones en mensajes DAO en la conexion a la BD

--- src/main/java/com/mycompany/mavenproject1/MensajesDAO.java
+++ src/main/java/com/mycompany/mavenproject1/MensajesDAO.java
@@ -4,13 +4,32 @@
  */
 package com.mycompany.mavenproject1;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author carolan
  */
 public class MensajesDAO {
     public static void crearMensajeDB(Mensajes mensaje){
-        
+        Conexion dbConnect = new Conexion();
+        try (Connection conexion = dbConnect.get_connection()){
+            PreparedStatement ps = null;
+            try {
+                String query = "INSERT INTO mensajes_app (mensajes, autor_mensaje, fecha_mensaje) VALUES (?,?, CURRENT_DATE())";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutorMensaje());
+                ps.executeUpdate();
+                System.out.println("Mensaje Creado");
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
     public static void leerMensajesDB(){
         
--- src/main/java/com/mycompany/mavenproject1/MensajesService.java
+++ src/main/java/com/mycompany/mavenproject1/MensajesService.java
@@ -4,8 +4,22 @@
  */
 package com.mycompany.mavenproject1;
 
+import java.util.Scanner;
+
 public class MensajesService {
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un mensaje");
+        String mensaje = sc.nextLine();
+        
+        System.out.println("Escribe tu nombre");
+        String nombre = sc.nextLine();
+        
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutorMensaje(nombre);
+        
+        MensajesDAO.crearMensajeDB(registro);
         
     }
     public static void listarMensaje(){
