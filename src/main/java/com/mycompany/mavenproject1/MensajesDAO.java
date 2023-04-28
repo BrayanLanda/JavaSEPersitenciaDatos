9723d844b6cac51a13774f8af572e314662f43c6
Brayan Acosta <landakkpp@gmail.com>
2023-04-28 00:06:15 -0500

    agrenado clases para conexion con BD - creando estructura en inicio para menu despliegable usuario

---
+++ src/main/java/com/mycompany/mavenproject1/MensajesDAO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author carolan
+ */
+public class MensajesDAO {
+    public static void crearMensajeDB(Mensajes mensaje){
+        
+    }
+    public static void leerMensajesDB(){
+        
+    }
+    public static void borrarMensajeDB(int idMensaje){
+        
+    }
+    public static void actualizarMensajeDB(Mensajes mensaje){
+        
+    }
+}
